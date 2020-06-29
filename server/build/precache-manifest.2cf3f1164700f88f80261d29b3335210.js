38a224587dc9ac1e643905d5245bd55dc3308b84
chiragdoctor <chirag.doctor@gmail.com>
2020-06-29 18:18:54 +0100

    add prod build

---
+++ server/build/precache-manifest.2cf3f1164700f88f80261d29b3335210.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49459cff1502c6a37b3e6f1c41e91865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0b40ee20140b0410f04",
+    "url": "/static/css/main.f2811824.chunk.css"
+  },
+  {
+    "revision": "171ba7c9d6258a87bacf",
+    "url": "/static/js/2.5ea16791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ea16791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b40ee20140b0410f04",
+    "url": "/static/js/main.43438ee6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c056517dcb3ea6eadce7059d3137745d",
+    "url": "/static/media/bootcamp.c056517d.jpg"
+  }
+]);
